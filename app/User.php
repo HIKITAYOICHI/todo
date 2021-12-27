f3ef95ebbe901b1d0b3d7745d3e3640dba106746
HIKITA <sks96800629yahoo.co.jp>
2021-12-27 11:13:27 +0900

    Userモデルにリレーション追加/taskstableにuser_id追加

--- app/User.php
+++ app/User.php
@@ -36,4 +36,11 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+    
+    // Taskモデルとの１対多でのリレーション
+    public function tasks()
+    {
+        return $this->hasMany('App\Task');
+        
+    }
 }
