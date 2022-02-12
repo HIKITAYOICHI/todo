8ac006e84d7c5386db45425587f0d2e05a3a3739
HIKITA <sks96800629yahoo.co.jp>
2022-02-12 15:20:10 +0900

    ナビゲーションバー修正

--- resources/views/layouts/admin/admin.blade.php
+++ resources/views/layouts/admin/admin.blade.php
@@ -21,63 +21,64 @@
     <link href="{{ asset('css/admin.css') }}" rel="stylesheet">
 </head>
 <body>
-        <div id="app">
-            {{-- 画面上部に表示するナビゲーションバー --}}
-            <nav class="navbar navbar-expand-md navbar-dark bg-dark shadow-sm">
-                <div class="container">
-                    <a class="navbar-brand" href="{{ url('/') }}">{{ config('app.name', 'Task管理ツール') }}</a>
-                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                        <span class="navbar-toggler-icon"></span>
-                    </button>
-    
-                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                        <!-- Left Side Of Navbar -->
-                        <ul class="navbar-nav mr-auto">
-    
-                        </ul>
-    
-                        <!-- Right Side Of Navbar -->
-                        <ul class="navbar-nav ml-auto">
-                            {{-- ナビゲーションバーにリンクの表示 --}}
-                            <!-- Authentication Links -->
-                            {{-- ログインしていなかったらログイン画面へのリンクを表示 --}}
-                            @guest
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('admin.login') }}">{{ __('ログイン') }}</a>
-                                </li>
-                                <!--@if (Route::has('register'))-->
-                                <!--    <li class="nav-item">-->
-                                <!--        <a class="nav-link" href="{{ route('admin.register') }}">{{ __('ユーザー登録') }}</a>-->
-                                <!--    </li>-->
-                                <!--@endif-->
-                            @else
-                                <li class="nav-item dropdown">
-                                    <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                        {{ Auth::user()->name }}
-                                    </a>
-    
-                                    <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                        <a class="dropdown-item" href="{{ route('admin.logout') }}"
-                                           onclick="event.preventDefault();
-                                                         document.getElementById('logout-form').submit();">
-                                            {{ __('ログアウト') }}
-                                        </a>
-    
-                                        <form id="logout-form" action="{{ route('admin.logout') }}" method="POST" class="d-none">
-                                            @csrf
-                                        </form>
-                                    </div>
-                                </li>
-                            @endguest
-                        </ul>
-                    </div>
+    <div id="app">
+        {{-- 画面上部に表示するナビゲーションバー --}}
+        <nav class="navbar navbar-expand-md navbar-dark bg-dark shadow-sm">
+            <div class="container">
+                <div class="toplink">
+                <a class="navbar-brand" href="{{ url('/') }}">{{ config('app.name', 'Task管理ツール') }}</a>
                 </div>
-            </nav>
-    
-            <main class="py-4">
-                @yield('content')
-            </main>
-        </div>
-       
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <!-- Left Side Of Navbar -->
+                    <ul class="navbar-nav mr-auto">
+
+                    </ul>
+
+                    <!-- Right Side Of Navbar -->
+                    <ul class="navbar-nav ml-auto">
+                        {{-- ナビゲーションバーにリンクの表示 --}}
+                        <!-- Authentication Links -->
+                        {{-- ログインしていなかったらログイン画面へのリンクを表示 --}}
+                        @guest
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('admin.login') }}">{{ __('ログイン') }}</a>
+                            </li>
+                            <!--@if (Route::has('register'))-->
+                            <!--    <li class="nav-item">-->
+                            <!--        <a class="nav-link" href="{{ route('admin.register') }}">{{ __('ユーザー登録') }}</a>-->
+                            <!--    </li>-->
+                            <!--@endif-->
+                        @else
+                            <li class="nav-item dropdown">
+                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                    {{ Auth::user()->name }}
+                                </a>
+
+                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="{{ route('admin.logout') }}"
+                                       onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                        {{ __('ログアウト') }}
+                                    </a>
+
+                                    <form id="logout-form" action="{{ route('admin.logout') }}" method="POST" class="d-none">
+                                        @csrf
+                                    </form>
+                                </div>
+                            </li>
+                        @endguest
+                    </ul>
+                </div>
+            </div>
+        </nav>
+
+        <main class="py-4">
+            @yield('content')
+        </main>
+    </div>
 </body>
 </html>
