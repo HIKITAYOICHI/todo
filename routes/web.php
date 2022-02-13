974ebfa86d4b47cc047c9f894b5d2b576f577aa2
HIKITA <sks96800629yahoo.co.jp>
2022-02-13 13:45:04 +0900

    ルーティング修正-commnetエラー解消

--- routes/web.php
+++ routes/web.php
@@ -35,22 +35,21 @@ Route::namespace('User')->prefix('user')->name('user.')->group(function () {
     // ログイン認証後
     Route::middleware('auth:user')->group(function () {
         // TOPページ
-        Route::resource('home', 'HomeController', ['only' => 'index']);
-        Route::get('home', 'HomeController@index')->name('home');
-        Route::get('tasks','TaskController@add');
-        Route::get('tasks/create','TaskController@add');
+        // Route::resource('home', 'HomeController', ['only' => 'index']);
+        // Route::get('home', 'HomeController@index')->name('home');
+        // Route::get('tasks','TaskController@add');
+        Route::get('tasks/create','TaskController@create');
         Route::post('tasks/create','TaskController@store');
         Route::get('tasks/edit', 'TaskController@edit');
         Route::post('tasks/edit', 'TaskController@update');
         Route::get('tasks/delete', 'TaskController@delete');
         Route::get('tasks', 'TaskController@index');
         Route::get('tasks/show/{id}', 'TaskController@show');
-        Route::get('tasks/store', 'CommentController@store')->name('tasks.store');
-        Route::post('tasks/store','CommentController@store');
-        Route::get('tasks/show/delete/{id}', 'CommentController@delete');
-        Route::post('tasks/delete/{id}', 'CommentController@delete');
-        Route::get('tasks/delete/{id}', 'ImageController@delete');
-        // Route::post('tasks/delete/{id}', 'ImageController@delete');
+        Route::get('tasks/comment/store', 'CommentController@store');
+        Route::post('tasks/comment/store','CommentController@store');
+        Route::get('tasks/comment/delete/{id}', 'CommentController@delete');
+        Route::get('tasks/image/delete/{id}', 'ImageController@delete');
+        
     });
 });
 
@@ -65,23 +64,21 @@ Route::namespace('Admin')->prefix('admin')->name('admin.')->group(function () {
     // ログイン認証後
     Route::middleware('auth:admin')->group(function () {
         // TOPページ
-        Route::resource('home', 'HomeController', ['only' => 'index']);
-        Route::get('home', 'HomeController@index')->name('home');
-        // Route::get('tasks','TaskController@add');
-        Route::get('tasks/create','TaskController@add');
-        Route::post('tasks/create','TaskController@store');
+        Route::get('tasks', 'TaskController@index');
         Route::get('tasks/edit', 'TaskController@edit');
         Route::post('tasks/edit', 'TaskController@update');
         Route::get('tasks/delete', 'TaskController@delete');
-        Route::get('tasks', 'TaskController@index');
         Route::get('tasks/show/{id}', 'TaskController@show');
-        Route::get('tasks/store', 'CommentController@store')->name('tasks.store');
-        Route::post('tasks/store','CommentController@store');
-        Route::get('tasks/show/delete/{id}', 'CommentController@delete');
-        Route::post('tasks/show/delete/{id}', 'CommentController@delete');
+        Route::get('tasks/comment/store', 'CommentController@store');
+        Route::post('tasks/comment/store','CommentController@store');
+        Route::get('tasks/comment/delete/{id}', 'CommentController@delete');
         // vendor/laravel/ui/auth-backend/RegistersUsers.php内のアクションから参照
-        Route::get('register', 'Auth\RegisterController@register')->name('register');
-        Route::post('register', 'Auth\RegisterController@register');
+        // Route::get('register', 'Auth\RegisterController@register')->name('register');
+        // Route::post('register', 'Auth\RegisterController@register');
+        // Route::resource('home', 'HomeController', ['only' => 'index']);
+        // Route::get('home', 'HomeController@index')->name('home');
+        // Route::get('tasks/create','TaskController@add');
+        // Route::post('tasks/create','TaskController@store');
         
     });
 });
